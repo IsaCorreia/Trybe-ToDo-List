7b139c1d489dc0d73a62bc111ea61c7ab49f596c
Isabela Correia <isabela.correiasilva00@gmail.com>
2021-11-23 16:16:19 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function addTask() {
     let item = document.createElement('li');
     let inputText = document.getElementById('texto-tarefa').value;
     item.innerText = inputText;
+    item.classList.add('item');
     document.getElementById('lista-tarefas').appendChild(item);
     document.getElementById('texto-tarefa').value = '';
 }
+
+function listClick(event) {
+    if (event.target.classList.contains('item') === true) {
+        let item = event.target;
+        item.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+}
+addEventListener('click', listClick);
