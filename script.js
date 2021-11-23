aeb4267792510f617574906977325b47704766b4
Isabela Correia <isabela.correiasilva00@gmail.com>
2021-11-23 16:02:37 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('Hello. World!');
+
+function addTask() {
+    let item = document.createElement('li');
+    let inputText = document.getElementById('texto-tarefa').value;
+    item.innerText = inputText;
+    document.getElementById('lista-tarefas').appendChild(item);
+    document.getElementById('texto-tarefa').value = '';
+}
