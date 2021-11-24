899952c65f1800f14fb6eadaabf6499af49f1e2b
Isabela Correia <isabela.correiasilva00@gmail.com>
2021-11-24 17:42:34 -0300

    Linter no script.js

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
 function addTask() {
-    let item = document.createElement('li');
-    let inputText = document.getElementById('texto-tarefa').value;
-    item.innerText = inputText;
-    item.classList.add('item');
+  const item = document.createElement('li');
+  const inputText = document.getElementById('texto-tarefa').value;
 
-    document.getElementById('lista-tarefas').appendChild(item);
-    document.getElementById('texto-tarefa').value = '';
+  item.innerText = inputText;
+  item.classList.add('item');
+  document.getElementById('lista-tarefas').appendChild(item);
+  document.getElementById('texto-tarefa').value = '';
 }
 
-function selectedTask (event){
-    // Define newItem como o elemento clicado
-    const newItem = event.target;
-    // Define old Item como o elemento que tem a classe selected
-    const oldItem = document.querySelector('.selected');
+// Define o clique como newItem
+// Define todos os itens com a classe 'selected' como allSelected
+// Define o primeiro elemento com a classe 'selected' como oldItem
+// Confere se o elemento clicado contém a classe 'item' (limita os cliques aos elementos da lista)
+// Compara se existem elementos com a classe 'selected' e se o item clicado contém a classe 'selected'
+// IF: Atualiza as classes de newItem e oldItem, enquanto troca a cor do fundo dos itens
+// ELSE: Caso o item clicado não tenha a classe 'selected' (primeira interação), adiciona e troca a cor de fundo
+function selectedTask(event) {
+  const newItem = event.target;
+  const oldItem = document.querySelector('.selected');
+  const allSelected = document.querySelectorAll('.selected');
 
-    // Limita os cliques aos elementos com classe 'item'
-    if (newItem.classList.contains('item') === true) {
-        // Se: selected tiver mais que um elemento E newItem não tiver selected
-        if (document.querySelectorAll('.selected').length === 1 && newItem.classList.contains('selected') == false) {
-            // adiciona .selected a newItem, remove de oldItem
-            newItem.classList.add('selected'); 
-            oldItem.classList.remove('selected');
-
-            // Estiliza o newItem em cinza, oldItem em branco
-            newItem.style.backgroundColor = 'rgb(128, 128, 128)';
-            oldItem.style.backgroundColor = 'white';
-        } else {
-
-            // Caso não tiver nenhum selected, adiciona a classe
-            newItem.classList.add('selected'); 
-            newItem.style.backgroundColor = 'rgb(128, 128, 128)';
-        }
+  if (newItem.classList.contains('item') === true) {
+    if (allSelected.length === 1 && newItem.classList.contains('selected')) {
+      newItem.classList.add('selected');
+      oldItem.classList.remove('selected');
+      newItem.style.backgroundColor = 'rgb(128, 128, 128)';
+      oldItem.style.backgroundColor = 'white';
+    } else {
+      newItem.classList.add('selected');
+      newItem.style.backgroundColor = 'rgb(128, 128, 128)';
     }
+  }
 }
 addEventListener('click', selectedTask);
 
-function checkTask (event) {
-    const task = event.target;
-    // Filtra evento de clique somente para os itens da lista
-    if (task.classList.contains('item') === true) {
-        // Se o item NÃO contém a classe "completed"
-        if (task.classList.contains('completed') === false) {
-            // Adiciona a classe ao item
-            task.classList.add('completed');
+// Filtra evento de clique somente para os itens da lista
+// IF: item NÃO contém a classe "completed", adiciona a classe ao item
+// ELSE IF: item CONTÉM a classe "completed", remove a classe do item
+function checkTask(event) {
+  const task = event.target;
 
-        // Se o item CONTÉM a classe "completed"
-        } else if (task.classList.contains('completed') === true) {
-            // Remove a classe do item
-            task.classList.remove('completed');
-        }
+  if (task.classList.contains('item') === true) {
+    if (task.classList.contains('completed') === false) {
+      task.classList.add('completed');
+    } else if (task.classList.contains('completed') === true) {
+      task.classList.remove('completed');
     }
+  }
 }
 addEventListener('dblclick', checkTask);
 
 function clearAll() {
-    const list = document.getElementById('lista-tarefas');
-    list.innerHTML='';
+  const list = document.getElementById('lista-tarefas');
+  list.innerHTML = '';
+}
+
+function removeElements() {
+  const list = document.getElementById('lista-tarefas').children;
+  for (const item of list) {
+    // Se o elemento filho tiver a classe completed
+    if (item.classList.contains('completed') === true) {
+      // remove o elemento
+      item.remove();
+    }
+  }
 }
 
 function clearCompleted() {
-    const list = document.getElementById('lista-tarefas').children;
-
-    while (document.getElementsByClassName('completed').length !== 0) {
-        // Percorre a lista, retornando o elemento filho em si
-        for (let item of list) {
-            // Se o elemento filho tiver a classe completed
-            if (item.classList.contains('completed') === true) {
-                // remove o elemento
-                item.remove();
-            }
-        }
-    }
+  // Enquanto a quantidade de elementos com a classe 'completed' for diferente de zero
+  while (document.getElementsByClassName('completed').length !== 0) {
+    removeElements();
+  }
 }
